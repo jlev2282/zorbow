8ffc93fb088dcefb41a1af83117dd15cfdbc13a8
John Leverett <jleverett@gls.com>
2018-08-15 15:36:48 -0400

    able to create new room unders rooms collection

--- chat.js
+++ chat.js
@@ -9,6 +9,7 @@ var config = {
     messagingSenderId: "224909053616"
 };
 firebase.initializeApp(config);
+var database = firebase.database();
 
 //use this to make a call to database and get info for tab selected
 $(".chat_selection").on("click", function(event){
@@ -17,5 +18,33 @@ $(".chat_selection").on("click", function(event){
     console.log(thisEvent.currentTarget.dataset.pick);
     //get the data from the user chats out of firebase
     //for each one create a button that has the chat name and users in chat
+
+});
+
+$("#create_room").on("click", function(event){
+    rooms = database.ref("/rooms");
+
+    event.preventDefault();
+
+    title = $("#roomTitle").val().trim();
+    category = $("#roomCategorySelect").val().trim();
+    description = $("#roomDescription").val().trim();
+
+    var r =confirm("Create room "+title+"?");
+
+    if (r == true) {
+        var newRoom = {
+            title: title,
+            category: category,
+            description: description
+        };
+
+        rooms.push(newRoom);
+    } else {
+        $("#roomTitle").val("");
+        $("#roomCategorySelect").val("");
+        $("#roomDescription").val("");
+    }
+
 
 });
