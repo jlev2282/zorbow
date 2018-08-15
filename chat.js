b1268db5c51f416ef5906c44701fae2904546647
John Leverett <jleverett@gls.com>
2018-08-15 12:14:45 -0400

    added chat page layout

---
+++ chat.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDwZ9TkDieYI4AFopofxnHjoCy95vvpdFc",
+    authDomain: "b-b-room.firebaseapp.com",
+    databaseURL: "https://b-b-room.firebaseio.com",
+    projectId: "b-b-room",
+    storageBucket: "b-b-room.appspot.com",
+    messagingSenderId: "224909053616"
+};
+firebase.initializeApp(config);
